db2f4c68aa24749017639dab628df837326e8417
16Maarten <74781319+16Maarten@users.noreply.github.com>
2021-01-23 21:22:29 +0100

    dropdown menus toegevoegd en database koppeling gemaakt

--- src/ui/InputCertificate.java
+++ src/ui/InputCertificate.java
@@ -2,10 +2,13 @@ package ui;
 
 import java.util.ArrayList;
 
+import DatabaseConnection.Communication;
+import javafx.collections.FXCollections;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
@@ -13,27 +16,36 @@ import person.Certificate;
 
 public class InputCertificate {
     private ArrayList<Certificate> list;
-
-    public InputCertificate(Certificate certificate){
+    private Communication com = new Communication();
+    private ArrayList<String> courses = new ArrayList<>();
+    private ArrayList<String> dates = new ArrayList<>();
+    private ArrayList<String> emails = new ArrayList<>();
+    public InputCertificate(Certificate certificate) {
         this.list = new ArrayList<>();
         this.list.add(certificate);
     }
 
-    public Parent getView(){
+    public Parent getView() {
         GridPane layout = new GridPane();
 
+        String courseQuery = "SELECT CursusName FROM Registration";
+        this.courses = com.getListFromDatabase(courseQuery, "CursusName");
+        String dateQuery = "SELECT RegistrationDate FROM Registration";
+        this.dates = com.getListFromDatabase(dateQuery, "RegistrationDate");
+        String emailQuery = "SELECT Email FROM Registration";
+        this.emails = com.getListFromDatabase(emailQuery, "Email");
         Label nameInstruction = new Label("Cursus Naam");
-        TextField nameField = new TextField();
+        ComboBox nameField = new ComboBox(FXCollections .observableArrayList(this.courses));
         Label gradeInstruction = new Label("Cijfer");
         TextField gradeField = new TextField();
         Label emailInstruction = new Label("Email");
-        TextField emailField = new TextField();
+        ComboBox emailField = new ComboBox(FXCollections .observableArrayList(this.emails));
         Label employeeInstruction = new Label("Naam medewerker");
         TextField employeeField = new TextField();
         Label dateInstruction = new Label("Registratie datum");
-        TextField dateField = new TextField();
-        
+        ComboBox dateField = new ComboBox(FXCollections .observableArrayList(this.dates));
 
+        System.out.println(this.dates);
         layout.setAlignment(Pos.CENTER);
         layout.setVgap(10);
         layout.setHgap(10);
@@ -50,25 +62,21 @@ public class InputCertificate {
         layout.add(employeeInstruction, 0, 6);
         layout.add(employeeField, 0, 7);
         layout.add(dateInstruction, 0, 8);
-        layout.add(dateField, 0, 9);
+        layout.add(dateField, 0, 9); 
         layout.add(addButton, 0, 10);
-       
 
         addButton.setOnMouseClicked((event) -> {
-            String name = nameField.getText();
+            String name = (String) nameField.getValue();
             String grade = gradeField.getText();
-            String email = emailField.getText();
+            String email = (String) emailField.getValue();
             String employee = employeeField.getText();
-            String date = dateField.getText();
-            
+            String date = (String) dateField.getValue();
+            String SQL = "INSERT INTO Certificate(Grade, EmployeeName, RegistrationDate, Email, CursusName) VALUES('"+ grade + "','" + employee + "','" + date + "','" + email + "','" + name + "')";
+            System.out.println(SQL);
+            com.addToDatabase(SQL);
 
-            //list.add(name, subject, intro, difficulty);
-
-            nameField.clear();
             gradeField.clear();
-            emailField.clear();
             employeeField.clear();
-            dateField.clear();
         });
 
         return layout;
