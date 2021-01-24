4b59cde0262f0afc1451d853da2fdf970f83b06b
16Maarten <74781319+16Maarten@users.noreply.github.com>
2021-01-24 14:14:19 +0100

    Werkend gemaakt

--- src/ui/GenderView.java
+++ src/ui/GenderView.java
@@ -2,6 +2,7 @@ package ui;
 
 import java.util.ArrayList;
 
+import DatabaseConnection.Communication;
 import javafx.collections.FXCollections;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -13,7 +14,6 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import person.Certificate;
 import person.Cursist;
-import ui.Main;
 
 
 
@@ -21,60 +21,52 @@ import ui.Main;
 public class GenderView {
     int total = 0;
     int pass = 0;
-    private Main com = new Main();
+    private Communication com = new Communication();
     private ArrayList<String> genders = new ArrayList();
+    private ArrayList<String> allCertificates = new ArrayList();
 
     public GenderView(Cursist cursist){
-
-    }
-
-    public void getPercentage(Cursist cursist, Certificate certificate){
 
     }
 
     public Parent getView(){
         GridPane layout = new GridPane();
 
-        String genderQuery = "SELECT Gender FROM Cursist";
-        this.genders = com.getList1FromDatabase(genderQuery, "Gender");
+        String genderQuery = "SELECT Gender FROM Cursist GROUP BY Gender";
+        this.genders = com.getListFromDatabase(genderQuery, "Gender");
 
-        Label genderLabel = new Label("Gender (M / F)");
-        TextField genderTextField = new TextField();
+        Label genderLabel = new Label("Gender (M / V)");
         ComboBox genderField = new ComboBox(FXCollections.observableArrayList(this.genders));
-
+        Label answerField = new Label();
         layout.setAlignment(Pos.CENTER);
         layout.setVgap(10);
         layout.setHgap(10);
         layout.setPadding(new Insets(10, 10, 10, 10));
 
-        Button getButton = new Button("Get percentage");
+        Button getButton = new Button("Percentage");
 
         layout.add(genderLabel, 0, 0);
-        layout.add(genderTextField, 0, 1);
+        layout.add(genderField, 0, 1);
         layout.add(getButton, 0, 2);
+        layout.add(answerField, 0, 3);
 
         getButton.setOnMouseClicked((event) -> {
-            String gender = genderTextField.getText();
-            String gender1 = (String) genderField.getValue();
-
-            String SQL = 
-            "(SELECT Name, Cursist.Email, CertificateId FROM Cursist JOIN Registration ON Registration.Email = Cursist.Email JOIN Cursus ON Cursus.CursusName = Registration.CursusName LEFT JOIN Certificate ON Certificate.RegistrationDate = Registration.RegistrationDate WHERE gender = this.gender)";
-            System.out.println(SQL);  
-            com.addToDatabase(SQL);
-            for(int i = 0; i < gender.length(); i++){
-                for(int j = 0; j < gender1.length(); j++){
-                    int percentage;
-                    percentage = (j / i) * 100;
-                    System.out.println(percentage);
-                }
-            }
-        
-            genderTextField.clear();
-        });
+            String gender = (String) genderField.getValue();
+            String allCertificatesQuery = "(SELECT Registration.Email FROM Cursist JOIN Registration ON Registration.Email = Cursist.Email JOIN Cursus ON Cursus.CursusName = Registration.CursusName WHERE gender = '" + gender +"')";
+            String SQL = "(SELECT CertificateId FROM Cursist JOIN Registration ON Registration.Email = Cursist.Email JOIN Cursus ON Cursus.CursusName = Registration.CursusName JOIN Certificate ON Certificate.RegistrationDate = Registration.RegistrationDate WHERE gender = '" + gender + "')";
+            this.genders = com.getListFromDatabase(SQL, "CertificateId");
+            this.allCertificates = com.getListFromDatabase(allCertificatesQuery , "Email");
+            double certificates = this.genders.size();
+            double everyone = this.allCertificates.size();
+            double answer = (certificates / everyone) * 100;
+            System.out.println(certificates);
+            System.out.println(everyone);
+            System.out.println(answer);
+            String answerText = String.valueOf(answer);
+            answerField.setText(answerText);
+            });
 
         return layout;
-
-
 
     }
 }
